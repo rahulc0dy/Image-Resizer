036348b693aea434412a557fc1ff7b500eb2eb27
Rahul chakraborty <rahulchakraborty337@gmail.com>
2023-11-06 22:33:50 +0530

    Image loading added

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,24 +1,34 @@
 // Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
 
-const form = document.querySelector('#img-form');
+const form = document.querySelector("#img-form");
+const img = document.querySelector("#img");
+const outputPath = document.querySelector("#output-path");
+const fileName = document.querySelector("#filename");
+const widthInput = document.querySelector("#width");
+const heightInput = document.querySelector("#height");
 
 function loadImage(e) {
-  const file = e.target.files[0];
+    const file = e.target.files[0];
 
-  if (!isFileImage(file)) {
-      alert('Please select an image file');
+    if (!isFileImage(file)) {
+        alert("Please select an image file");
         return;
-  }
+    }
+    // Get original Dimentsions
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    image.onload = function () {
+        widthInput.value = this.width;
+        heightInput.value = this.height;
+    };
 
-  form.style.display = 'block';
-  document.querySelector(
-    '#filename'
-  ).innerHTML = file.name;
+    form.style.display = "block";
+    fileName.innerHTML = file.name;
 }
 
 function isFileImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-    return file && acceptedImageTypes.includes(file['type'])
+    const acceptedImageTypes = ["image/gif", "image/jpeg", "image/png"];
+    return file && acceptedImageTypes.includes(file["type"]);
 }
 
-document.querySelector('#img').addEventListener('change', loadImage);
+img.addEventListener("change", loadImage);
